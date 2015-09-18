d8a9fc55274b8337ac930a1a2c2cbf789cef059c
n01035 <n01035@cybird-52961.local>
2015-09-18 20:02:35 +0900

    修正

--- src/run.py
+++ src/run.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
+import json
 import tools
-import create_tables
-import drop_tables
+from s3_table import S3Table
 
 def main():
     # 引数取得
     options = tools.get_argv()
+    options.setting_files = options.setting_files.split(",")
 
-    if options.create_flg == True:
-        # テーブル作成
-        create_tables.main()
+    for setting_file in options.setting_files:
+        settings = json.load(open(setting_file, "r"))
+        instance = S3Table(**settings)
 
-    if options.drop_flg == True:
-        # テーブル削除
-        drop_tables.main()
+        if options.create_flg == True:
+            # テーブル作成
+            instance.setup()
+
+        if options.drop_flg == True:
+            # テーブル削除
+            instance.drop_table()
 
     return 0
 
